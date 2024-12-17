5e80f23f1043b0b0e4bfdec232c5a2d36e52d003
zmskv <zmskvxd@gmail.com>
2024-12-17 15:01:11 +0300

    lab2 done

--- lab2.py
+++ lab2.py
@@ -1,167 +1,75 @@
+import math
 import numpy as np
-import sympy as sp
-from numba import njit
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import time
+from matplotlib.animation import FuncAnimation
 
-class Particle:
-    def __init__(self, r_expr, phi_expr, t):
-        self.t = t
-        self.r_expr = r_expr
-        self.phi_expr = phi_expr
-        self.r_func = sp.lambdify(t, r_expr, 'numpy')
-        self.phi_func = sp.lambdify(t, phi_expr, 'numpy')
-        self.r_dot_expr = sp.diff(r_expr, t)
-        self.phi_dot_expr = sp.diff(phi_expr, t)
-        self.r_dot_func = sp.lambdify(t, self.r_dot_expr, 'numpy')
-        self.phi_dot_func = sp.lambdify(t, self.phi_dot_expr, 'numpy')
+# Параметры времени и шагов
+num_steps = 1000
+time = np.linspace(0, 10, num_steps)
 
-    @staticmethod
-    @njit
-    def compute_position(r, phi):
-        x = r * np.cos(phi)
-        y = r * np.sin(phi)
-        return x, y
+# Углы
+angle1 = np.cos(time)
+angle2 = np.sin(time)
 
-    @staticmethod
-    @njit
-    def compute_velocity(r, phi, r_dot, phi_dot):
-        vx = r_dot * np.cos(phi) - r * phi_dot * np.sin(phi)
-        vy = r_dot * np.sin(phi) + r * phi_dot * np.cos(phi)
-        return vx, vy
+# Радиусы и параметры системы
+radius_main = 1
+radius_inner1 = 0.125
+radius_inner2 = 0.05
+a = radius_main - radius_inner2
+b = radius_main - radius_inner1
 
-    @staticmethod
-    @njit
-    def compute_acceleration(r, phi, r_dot, phi_dot, r_ddot, phi_ddot):
-        ax = (r_ddot - r * phi_dot**2) * np.cos(phi) - (2 * r_dot * phi_dot + r * phi_ddot) * np.sin(phi)
-        ay = (r_ddot - r * phi_dot**2) * np.sin(phi) + (2 * r_dot * phi_dot + r * phi_ddot) * np.cos(phi)
-        return ax, ay
+# Анимация
+fig = plt.figure()
+axis = fig.add_subplot(1, 1, 1)
+axis.axis('equal')
 
-    def get_positions(self, T):
-        r = self.r_func(T)
-        phi = self.phi_func(T)
-        x, y = self.compute_position(r, phi)
-        return x, y
+def rotate_2D(x, y, angle):
+    new_x = x * np.cos(angle) - y * np.sin(angle)
+    new_y = x * np.sin(angle) + y * np.cos(angle)
+    return new_x, new_y
 
-    def get_velocities(self, T):
-        r = self.r_func(T)
-        phi = self.phi_func(T)
-        r_dot = self.r_dot_func(T)
-        phi_dot = self.phi_dot_func(T)
-        vx, vy = self.compute_velocity(r, phi, r_dot, phi_dot)
-        return vx, vy
+# Центр окружности
+center_x, center_y = 0, 1
 
-    def get_accelerations(self, T):
-        r = self.r_func(T)
-        phi = self.phi_func(T)
-        r_dot = self.r_dot_func(T)
-        phi_dot = self.phi_dot_func(T)
-        r_ddot_expr = sp.diff(self.r_dot_expr, t)
-        phi_ddot_expr = sp.diff(self.phi_dot_expr, t)
-        r_ddot_func = sp.lambdify(t, r_ddot_expr, 'numpy')
-        phi_ddot_func = sp.lambdify(t, phi_ddot_expr, 'numpy')
-        r_ddot = r_ddot_func(T)
-        phi_ddot = phi_ddot_func(T)
-        ax, ay = self.compute_acceleration(r, phi, r_dot, phi_dot, r_ddot, phi_ddot)
-        return ax, ay
+# Центральный элемент
+central_circle = plt.Circle((center_x, center_y), radius_inner2, color='black')
 
-class Arrow:
-    def __init__(self, ax, x, y, dx, dy, color='k', scale=1.0):
-        self.ax = ax
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-        self.color = color
-        self.scale = scale
-        self.arrow = self.ax.quiver(self.x, self.y, self.dx * self.scale, self.dy * self.scale, color=self.color, angles='xy', scale_units='xy', scale=1)
+# Координаты первого подвижного элемента
+circle1_x = np.zeros(num_steps)
+circle1_y = np.zeros(num_steps)
 
-    def update(self, x, y, dx, dy):
-        self.arrow.remove()
-        self.arrow = self.ax.quiver(x, y, dx * self.scale, dy * self.scale, color=self.color, angles='xy', scale_units='xy', scale=1)
+for i in range(len(angle1)):
+    new_x, new_y = rotate_2D(0, -a, angle1[i])
+    circle1_x[i] = center_x + new_x
+    circle1_y[i] = center_y + new_y
 
-class Plotter:
-    def __init__(self, ax):
-        self.ax = ax
-        self.line, = ax.plot([], [], 'o', lw=2)
-        self.trace, = ax.plot([], [], '-', lw=1)
-        self.velocity_arrow = Arrow(ax, 0, 0, 0, 0, color='r', scale=0.1)
-        self.acceleration_arrow = Arrow(ax, 0, 0, 0, 0, color='g', scale=0.01)
-        self.legend_elements = [
-            plt.Line2D([0], [0], color='r', lw=2, label='Velocity'),
-            plt.Line2D([0], [0], color='g', lw=2, label='Acceleration')
-        ]
-        self.ax.legend(handles=self.legend_elements, loc='upper left')
+# Координаты второго подвижного элемента
+circle2_x = np.zeros(num_steps)
+circle2_y = np.zeros(num_steps)
 
-    def init(self):
-        self.line.set_data([], [])
-        self.trace.set_data([], [])
-        self.velocity_arrow.update(0, 0, 0, 0)
-        self.acceleration_arrow.update(0, 0, 0, 0)
-        return self.line, self.trace, self.velocity_arrow.arrow, self.acceleration_arrow.arrow
+for i in range(len(angle1)):
+    new_x, new_y = rotate_2D(0, -b, angle2[i])
+    circle2_x[i] = circle1_x[i] + new_x
+    circle2_y[i] = circle1_y[i] + new_y
 
-    def update(self, x, y, trace_x, trace_y, vx, vy, ax, ay):
-        self.line.set_data([x], [y])
-        self.trace.set_data(trace_x, trace_y)
-        self.velocity_arrow.update(x, y, vx, vy)
-        self.acceleration_arrow.update(x, y, ax, ay)
-        return self.line, self.trace, self.velocity_arrow.arrow, self.acceleration_arrow.arrow
+# Добавление центрального круга
+axis.add_patch(central_circle)
 
-    def reset_plot(self):
-        self.line.set_data([], [])
-        self.trace.set_data([], [])
-        self.velocity_arrow.update(0, 0, 0, 0)
-        self.acceleration_arrow.update(0, 0, 0, 0)
+# Добавление двух движущихся кругов
+moving_circle1 = plt.Circle((circle1_x[0], circle1_y[0]), radius_main, color='black', fill=False)
+moving_circle2 = plt.Circle((circle2_x[0], circle2_y[0]), radius_inner1, color='black', fill=False)
 
-class Animator:
-    def __init__(self, particle, plotter, T):
-        self.particle = particle
-        self.plotter = plotter
-        self.T = T
-        self.x, self.y = particle.get_positions(T)
-        self.vx, self.vy = particle.get_velocities(T)
-        self.ax, self.ay = particle.get_accelerations(T)
-        self.trace_x, self.trace_y = [], []
+axis.add_patch(moving_circle1)
+axis.add_patch(moving_circle2)
 
-    def init(self):
-        return self.plotter.init()
+# Функция анимации
+def animate(i):
+    moving_circle1.center = (circle1_x[i], circle1_y[i])
+    moving_circle2.center = (circle2_x[i], circle2_y[i])
 
-    def animate(self, i):
-        x = self.x[i]
-        y = self.y[i]
-        vx = self.vx[i]
-        vy = self.vy[i]
-        ax = self.ax[i]
-        ay = self.ay[i]
-        self.trace_x.append(x)
-        self.trace_y.append(y)
-        return self.plotter.update(x, y, self.trace_x, self.trace_y, vx, vy, ax, ay)
+axis.set(xlim=[-2, 2], ylim=[-2, 2])
 
-    def start(self, interval=20):
-        ani = animation.FuncAnimation(
-            self.plotter.ax.figure, self.animate, init_func=self.init,
-            frames=len(self.T), interval=interval, blit=True
-        )
-        plt.show(block=False)  # Не блокировать выполнение после показа графика
-        plt.pause(len(self.T) * interval / 1000 + 2)  # Пауза на время анимации + 2 секунды
-        plt.close()  # Закрыть окно с графиком
-        self.plotter.reset_plot()  # Сбросить график
 
-if __name__ == "__main__":
-    t = sp.Symbol('t')
-    r_expr = 2 + sp.sin(12 * t)
-    phi_expr = t + 0.2 * sp.cos(13 * t)
+animation = FuncAnimation(fig, animate, frames=num_steps, interval=1)
 
-    T = np.linspace(1, 10, 1000)
-
-    particle = Particle(r_expr, phi_expr, t)
-
-    fig, ax = plt.subplots()
-    ax.set_xlim(-3, 3)
-    ax.set_ylim(-3, 3)
-    ax.set_aspect('equal')
-
-    plotter = Plotter(ax)
-    animator = Animator(particle, plotter, T)
-    animator.start()
+plt.show()
